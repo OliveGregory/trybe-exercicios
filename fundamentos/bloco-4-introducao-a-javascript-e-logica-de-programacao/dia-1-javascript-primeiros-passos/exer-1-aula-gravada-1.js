6ec325e53e35ceb06c529738fca54a06c2c47d57
Gregory <gregorysak90@gmail.com>
2021-08-26 10:16:41 -0300

    Reorganizando diretórios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exer-1-aula-gravada-1.js
@@ -0,0 +1,9 @@
+const myName = 'Gregory';
+const birthCity = 'Santo Antônio de Jesus';
+let birthYear = 2000;
+
+birthCity = 'Lage';
+
+console.log(birthCity);
+console.log(myName);
+console.log(birthYear);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exer-5-aula-gravada-4.js
@@ -0,0 +1,10 @@
+let weekDay = 'sabado';
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay ==="sexta-feira") {
+	console.log('Oba, mais um dia de aprendizado');
+
+}
+
+else if (weekDay === "sabado" || weekDay === "domingo"){
+	console.log('FINALMENTE, descando merecido UwU');
+}
