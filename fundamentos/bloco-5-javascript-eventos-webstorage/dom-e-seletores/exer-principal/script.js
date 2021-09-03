017274af160a451ff17083d0c16529b0582d2d5a
Gregory <gregorysak90@gmail.com>
2021-09-02 21:28:46 -0300

    Requisito 3, exer-principla, feito

--- fundamentos/bloco-5-javascript-eventos-webstorage/dom-e-seletores/exer-principal/script.js
+++ fundamentos/bloco-5-javascript-eventos-webstorage/dom-e-seletores/exer-principal/script.js
@@ -1,3 +1,6 @@
 document.getElementsByTagName('p')[0].innerText = 'Me vejo feliz e bem empregado';
 
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+
+
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
