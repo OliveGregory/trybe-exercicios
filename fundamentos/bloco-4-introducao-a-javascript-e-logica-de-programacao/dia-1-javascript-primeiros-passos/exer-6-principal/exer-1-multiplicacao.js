8c722ba2a98a440c39265aa2a7b2a43daf2958a2
Gregory <gregorysak90@gmail.com>
2021-09-08 08:19:01 -0300

    terminado exercícios e organizando diretótio

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exer-6-principal/exer-1-multiplicacao.js
@@ -0,0 +1,6 @@
+const a = 15;
+const b = 3;
+
+let multiplicacao = (a*b);
+
+console.log(multiplicacao);
---
+++ fundamentos/bloco-5-javascript-eventos-webstorage/dia1-dom-e-seletores/base-exe-1.js
@@ -0,0 +1,5 @@
+/*document.getElementById("page-title").innerText = "John Wick"*/
+
+/*document.getElementById("paragraph").innerText = 'Hoje temm festa de gado'*/
+
+/*document.getElementById("second-paragraph").innerText = 'Exercício de getElement'*/
