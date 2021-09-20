d49997ad389bc09a07921295e5fc690c678f7ec9
Gregory <gregorysak90@gmail.com>
2021-09-20 10:31:13 -0300

    Exercício feito

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/new-script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/new-script.js
@@ -40,5 +40,47 @@ let arrayStates = [
     }
   }
     
-  array(); 
-  //console.log(arrayStates);
+array(); 
+
+
+function checkDate(){
+  // essa fun tem que ser chamada quando o botão de envio for clicado
+  let date = document.getElementById('date');
+  
+  if(date.value == ''){
+    alert('Por favor, informe a data no campo requerido!');
+  }
+
+}
+checkDate();
+
+
+function stop (e){
+  e.preventDefault()
+}
+
+
+let codeBody = document.getElementById('corpo');
+let bodyChild = codeBody.childNodes;
+let dateSpace = document.getElementById('date');
+function checkChildValue(){
+  // essa função devee ser chamada quando o botão de enviar for clicado
+  let compilationInfos = document.createElement('div');
+  let spaceError = document.createElement('div')
+
+  for(let i = 0; i < bodyChild.length; i += 1){
+    if(bodyChild[i].value !== ''){
+      compilationInfos.innerText(bodyChild[i])
+    }
+  }
+
+  if(dateSpace.value == ''){
+    spaceError.innerText = 'Error na data de início, preencha corretamente!';
+  }
+}
+
+let buttonCheck = document.getElementById('checkform');
+
+buttonCheck.addEventListener('click',checkDate)
+buttonCheck.addEventListener('click',checkChildValue)
+buttonCheck.addEventListener('click',stop);
