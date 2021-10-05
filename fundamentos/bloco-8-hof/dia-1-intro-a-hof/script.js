e7f1a36feec7d70312624473cef1611330dbcc0a
Gregory <gregorysak90@gmail.com>
2021-10-05 18:30:14 -0300

    Exercício iniciado

---
+++ fundamentos/bloco-8-hof/dia-1-intro-a-hof/script.js
@@ -0,0 +1,10 @@
+const newEmployees = () => {
+  const employees = {
+    id1: 'Pedro Guerra', 
+    id2: 'Luiza Drumond',
+    id3: 'Carla Paiva',
+  };
+  console.log(employees.toLowerCase());
+};
+
+newEmployees();
